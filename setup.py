b5cc8a8dcae6c2b26a9f1eef641e5f4b92f88e6e
varun <varunkumar2004.vk@gmail.com>
2025-01-18 12:21:11 +0530

    final version of 0.2.0 created with wheel setup files

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.3'
+VERSION = '0.2.0'
 DESCRIPTION = 'Technique for Order Preference by Similarity to Ideal Solution(TOPSIS) Package'
 
 # Setting up
